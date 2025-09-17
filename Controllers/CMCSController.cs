1fb01a3e33c14580c4e7f1f6f4ed56f5ae64b072
ST10446545-WiehanSmit <164011517+ST10446545-WiehanSmit@users.noreply.github.com>
2025-09-17 20:38:14 +0200

    Add project files.

---
+++ Controllers/CMCSController.cs
@@ -0,0 +1,17 @@
+﻿//*****************************************************************************
+//PROG6212_Part1_ST10446545_ST10446545@vcconnect.edu.za
+//*****************************************************************************
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCSProject.Controllers
+{
+    public class CMCSController : Controller
+    {
+        public IActionResult Dashboard() => View();
+        public IActionResult SubmitClaim() => View();
+        public IActionResult UploadedDocuments() => View();
+        public IActionResult Approvals() => View();
+        public IActionResult Profile() => View();
+    }
+}
+//---------------------------------------------------------- End Of File -----------------------------------------------------------------
